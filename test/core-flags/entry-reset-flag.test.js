7db747ed09fc27817e6a39031ecff5e61e9f9fe4
Nitin Kumar <snitin315@gmail.com>
2020-11-05 15:07:44 +0530

    tests: entry reset

--- test/core-flags/entry-reset-flag.test.js
+++ test/core-flags/entry-reset-flag.test.js
@@ -11,4 +11,12 @@ describe('--entry-reset flag', () => {
         expect(stdout).toContain('src/entry.js');
         expect(stdout).not.toContain('src/main.js');
     });
+
+    it('should thorw error if entry is an emtry array', () => {
+        const { stderr, stdout, exitCode } = run(__dirname, ['--entry-reset']);
+
+        expect(stderr).toContain('configuration.entry should be an non-empty array');
+        expect(exitCode).toBe(2);
+        expect(stdout).toBeFalsy();
+    });
 });
